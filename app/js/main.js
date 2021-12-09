b86737f202f2dd24e763b76a680f3a0367c5860a
Melkorich <Kirill.Lavrov92@gmail.com>
2021-12-09 19:44:18 +0300

    adaptive addded

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,21 @@ $(function(){
 
 
   $('.numbers__select').styler();
+
+
+  $('.menu__burger').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+    $('.menu__burger').toggleClass('menu__burger--active');
+    $('body').toggleClass('body--active');
+  });
+
+  $('.menu__link').on('click', function () {
+    $('.menu__list').removeClass('menu__list--active');
+    $('.menu__burger').removeClass('menu__burger--active');
+    $('body').removeClass('body--active');
+  });
  
+
+
+  
 }); 
